cafaf01cf3cfeabb447846245302f075426285b9
SUBI SHAH <subi.shah@getbraintree.com>
2018-10-17 19:34:09 -0500

    refactored controller and index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,24 @@
-
 class BooksController < ApplicationController
-
   require 'uri'
 
-  def searchbook
-      @book = params[:book]
-      if @book
-        my_search = make_url(@book)
-        search_by_title(my_search)
-      end
+  def index
+    @query = params[:q]
+    if @query
+      @books = search_by_title(@query)
     end
+  end
 
   def make_url(search)
-    # p search
-    searchparams = URI.escape(search.to_s)
-    search_url = "https://www.googleapis.com/books/v1/volumes?q=" + searchparams
-    @get_search = search_url
+    "https://www.googleapis.com/books/v1/volumes?q=" + URI.escape(search.to_s)
   end
 
-  def search_by_title(url)
+  def search_by_title(query)
+    url = make_url(query)
     response = HTTParty.get(url)
-    @books = response["items"]
+    if response["items"].present?
+      response["items"].map { |item| item["volumeInfo"] }
+    else
+      []
+    end
   end
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/searchbook'
-  get 'greetings/hello'
-  root 'books#searchbook'
-  post '/books/searchbook'
+  resources :books, {only: [:index]}
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
